aca68e311f2d511af27636e958c8a0eb77214227
Arun Gautam <arunraj1735169@gmail.com>
2019-11-16 14:45:34 +0530

    add all user view

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const {validateUser} = require("../db/models/user");
+const {validateUser, User} = require("../db/models/user");
 const db = require("../db/crudoperations/user");
 
 router.get('/form',(req, res)=>{
   res.render("index", {error: null, success: null})
 })
+
 router.get('/view',(req,res)=>{
   res.render("view")
-
 })
+
+router.get("/allResponse",  async (req, res)=>{
+  const allUsers = await User.find(); 
+  console.log(allUsers)
+  res.render("allUser", {allUsers})
+})
+
 router.get('/*',(req, res)=>{
    res.render("home")
 })
@@ -27,6 +34,6 @@ router.post('/user-form', async (req, res)=>{
   const dob=date.getDay()+"/"+date.getMonth()+1+'/'+date.getFullYear();
   res.render("view", {name:user.name,email:user.email,phoneNumber:user.phoneNumber, dob});
   
-}); 
+});
 
 module.exports = router;
